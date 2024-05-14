4f878fad5722ae430f7792c2a055c8d7b3df0a21
AndreaDF94 <168236894+AndreaDF94@users.noreply.github.com>
2024-05-14 21:30:15 +0200

    Add files via upload

---
+++ Le variabili in py.py
@@ -0,0 +1,17 @@
+#                                       Python
+
+# Le basi di Python 
+# Variabili
+# Le variabili sono come contenitori che possiamo utilizzare per memorizzare dati in Pyhton.
+# Possiamo utilizarle per conservare numeri, stringhe , elenchi di elementi e molto altro ancora.
+
+nome =  "Alice" # la parola tra gli apici vien definita come stringa o str
+età = 30 # qui abbiamo un numero intero|definito anche come int
+altezza = 160.3 # Contiene un numero decimale| definito anche come float
+studente = True # contiene un valore booleano
+# Usando print andiamo a stampare il contenuto di ciascuna vaiabile.
+# Le variabili in Python sono flessibili e possono essere assegnate e riassegnate con diversi tipi di dati.
+print("Nome: ", nome)
+print("Età: ", età)
+print("altezza:", altezza)
+print("studente", studente)
